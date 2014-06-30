00549093fd6aff6a8b311a633b3ca143f3de878e
Florian Rival <Florian.rival@gmail.com>
2014-06-30 21:11:32 +0200

    Updated docs

--- GDCore Documentation/search/pages_77.js
+++ GDCore Documentation/search/pages_77.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['write_20a_20new_20extension',['Write a new extension',['../_write_a_new_extension.html',1,'overview']]]
+  ['write_20a_20new_20extension',['Write a new extension',['../write_a_new_extension.html',1,'overview']]]
 ];
