0aa2ff6e70cd465c6737b5825bb8f0efd1e6ec17
Florian Rival <Florian.rival@gmail.com>
2014-12-29 18:25:10 +0100

    Updated docs.

--- GDCore Documentation/search/pages_73.js
+++ GDCore Documentation/search/pages_73.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['setting_20up_20the_20development_20environnement',['Setting up the development environnement',['../setup_dev_env.html',1,'index']]]
+  ['setting_20up_20the_20development_20environment',['Setting up the development environment',['../setup_dev_env.html',1,'index']]]
 ];
