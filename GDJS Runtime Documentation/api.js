5605293b932cb635e604361732f7ab037327a984
Florian Rival <Florian.Rival@gladeye.co.nz>
2015-04-13 21:11:39 +1200

    Updated docs

---
+++ GDJS Runtime Documentation/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "gdjs.EventsContext",
+        "gdjs.Force",
+        "gdjs.ImageManager",
+        "gdjs.InputManager",
+        "gdjs.Layer",
+        "gdjs.Polygon",
+        "gdjs.RuntimeAutomatism",
+        "gdjs.RuntimeGame",
+        "gdjs.RuntimeObject",
+        "gdjs.RuntimeScene",
+        "gdjs.Sound",
+        "gdjs.SoundManager",
+        "gdjs.SpriteAnimation",
+        "gdjs.SpriteAnimationFrame",
+        "gdjs.SpriteRuntimeObject",
+        "gdjs.Timer",
+        "gdjs.Variable",
+        "gdjs.VariablesContainer",
+        "gdjs.evtTools.camera",
+        "gdjs.evtTools.common",
+        "gdjs.evtTools.input",
+        "gdjs.evtTools.network",
+        "gdjs.evtTools.object",
+        "gdjs.evtTools.runtimeScene",
+        "gdjs.evtTools.sound",
+        "gdjs.evtTools.storage",
+        "gdjs.evtTools.string",
+        "gdjs.evtTools.window"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
