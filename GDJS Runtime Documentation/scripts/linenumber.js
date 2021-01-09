58a31222e14863189310e444bc2363db71b852b2
GDevelop documentation CI <ci-build@gdevelop-app.com>
2021-01-09 13:56:16 +0000

    Update documentation [ci skip]

--- GDJS Runtime Documentation/scripts/linenumber.js
+++ GDJS Runtime Documentation/scripts/linenumber.js
@@ -1,26 +1,23 @@
-/*global document */
+'use strict';
 
-(function() {
-    var source = document.getElementsByClassName('prettyprint source linenums');
-    var i = 0;
-    var lineNumber = 0;
-    var lineId;
-    var lines;
-    var totalLines;
-    var anchorHash;
+(function () {
+  let lineId, lines, totalLines, anchorHash;
+  const source = document.getElementsByClassName('prettyprint source linenums');
+  let i = 0;
+  let lineNumber = 0;
 
-    if (source && source[0]) {
-        anchorHash = document.location.hash.substring(1);
-        lines = source[0].getElementsByTagName('li');
-        totalLines = lines.length;
+  if (source && source[0]) {
+    anchorHash = document.location.hash.substring(1);
+    lines = source[0].getElementsByTagName('li');
+    totalLines = lines.length;
 
-        for (; i < totalLines; i++) {
-            lineNumber++;
-            lineId = 'line' + lineNumber;
-            lines[i].id = lineId;
-            if (lineId === anchorHash) {
-                lines[i].className += ' selected';
-            }
-        }
+    for (; i < totalLines; i++) {
+      lineNumber++;
+      lineId = 'line' + lineNumber;
+      lines[i].id = lineId;
+      if (lineId === anchorHash) {
+        lines[i].className += ' selected';
+      }
     }
+  }
 })();
