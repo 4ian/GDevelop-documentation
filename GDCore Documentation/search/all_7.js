67c30c3c8cb047abe58fc3b7c58b9bbdb8418c10
GDevelop documentation CI <ci-build@gdevelop-app.com>
2021-03-28 00:12:28 +0000

    Update documentation [ci skip]

--- GDCore Documentation/search/all_7.js
+++ GDCore Documentation/search/all_7.js
@@ -403,11 +403,12 @@ var searchData=
   ['grid',['grid',['../classgd_1_1_layout_editor_canvas_options.html#a7c171612cc3615add1a7929b3cf1a7f6',1,'gd::LayoutEditorCanvasOptions']]],
   ['gridb',['gridB',['../classgd_1_1_layout_editor_canvas_options.html#a8710ae85858f8d3a5619e56be7024fdf',1,'gd::LayoutEditorCanvasOptions']]],
   ['gridg',['gridG',['../classgd_1_1_layout_editor_canvas_options.html#a4eae0d0ad5d7689c0ef32d5fd273d6ef',1,'gd::LayoutEditorCanvasOptions']]],
-  ['gridheight',['gridHeight',['../classgd_1_1_layout_editor_canvas_options.html#ab8c884be1baa2514ab1c2ee41be0d567',1,'gd::LayoutEditorCanvasOptions']]],
-  ['gridoffsetx',['gridOffsetX',['../classgd_1_1_layout_editor_canvas_options.html#a7029e7a9ff4878f38703b95d14f222fc',1,'gd::LayoutEditorCanvasOptions']]],
-  ['gridoffsety',['gridOffsetY',['../classgd_1_1_layout_editor_canvas_options.html#a05be858738f2a40a02a328fa77e45bd5',1,'gd::LayoutEditorCanvasOptions']]],
+  ['gridheight',['gridHeight',['../classgd_1_1_layout_editor_canvas_options.html#a40610bd85999de3d510da0dc84459cd9',1,'gd::LayoutEditorCanvasOptions']]],
+  ['gridoffsetx',['gridOffsetX',['../classgd_1_1_layout_editor_canvas_options.html#a535d035dd1a8e03dec562a1971b019f4',1,'gd::LayoutEditorCanvasOptions']]],
+  ['gridoffsety',['gridOffsetY',['../classgd_1_1_layout_editor_canvas_options.html#a2aab86824dcd4740ebd2d60ae26ce423',1,'gd::LayoutEditorCanvasOptions']]],
   ['gridr',['gridR',['../classgd_1_1_layout_editor_canvas_options.html#a96ba4265574a6650008aed46b166dc7f',1,'gd::LayoutEditorCanvasOptions']]],
-  ['gridwidth',['gridWidth',['../classgd_1_1_layout_editor_canvas_options.html#a0b77d20dceee1d39698cda9ec2ab070e',1,'gd::LayoutEditorCanvasOptions']]],
+  ['gridtype',['gridType',['../classgd_1_1_layout_editor_canvas_options.html#a6206a71de23b15fcadbc35bc168e7376',1,'gd::LayoutEditorCanvasOptions']]],
+  ['gridwidth',['gridWidth',['../classgd_1_1_layout_editor_canvas_options.html#a3d79fd8d296aaaacdab8ccb6bbfeea44',1,'gd::LayoutEditorCanvasOptions']]],
   ['groupevent',['GroupEvent',['../classgd_1_1_group_event.html',1,'gd']]],
   ['gdevelop_20core',['GDevelop Core',['../index.html',1,'']]]
 ];
