c4dace0de9df9058c6889ad73ce94d5263b3042a
GDevelop documentation CI <ci-build@gdevelop-app.com>
2021-07-20 00:11:27 +0000

    Update documentation [ci skip]

--- GDJS Documentation/search/all_f.js
+++ GDJS Documentation/search/all_f.js
@@ -5,6 +5,7 @@ var searchData=
   ['setcodeoutputdirectory',['SetCodeOutputDirectory',['../dc/dda/classgdjs_1_1_exporter.html#a9c51541a1481d6bf610f15f1621e1ddd',1,'gdjs::Exporter::SetCodeOutputDirectory()'],['../d0/d75/classgdjs_1_1_exporter_helper.html#afeecd7cc427087d226bf9591954ef243',1,'gdjs::ExporterHelper::SetCodeOutputDirectory()']]],
   ['setdebuggerserveraddress',['SetDebuggerServerAddress',['../db/dbe/structgdjs_1_1_preview_export_options.html#ab5a0c149dd00f07a283087e9edae0283',1,'gdjs::PreviewExportOptions']]],
   ['setexternallayoutname',['SetExternalLayoutName',['../db/dbe/structgdjs_1_1_preview_export_options.html#a26226ff501d285d19288143f77581548',1,'gdjs::PreviewExportOptions']]],
+  ['setfullloadingscreen',['SetFullLoadingScreen',['../db/dbe/structgdjs_1_1_preview_export_options.html#a7bfbc8544c60894d6eb4575bf79b0889',1,'gdjs::PreviewExportOptions']]],
   ['setincludefilehash',['SetIncludeFileHash',['../db/dbe/structgdjs_1_1_preview_export_options.html#a4ad48abf2a9a6f3974b91c12ea1eb136',1,'gdjs::PreviewExportOptions']]],
   ['setlayoutname',['SetLayoutName',['../db/dbe/structgdjs_1_1_preview_export_options.html#a99b87ae4767666af213ef3ec3ba209ed',1,'gdjs::PreviewExportOptions']]],
   ['setnonruntimescriptscacheburst',['SetNonRuntimeScriptsCacheBurst',['../db/dbe/structgdjs_1_1_preview_export_options.html#ac4c9f5d4bdbf88903ff563b2381c3972',1,'gdjs::PreviewExportOptions']]],
--- GDJS Documentation/search/functions_0.js
+++ GDJS Documentation/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['addlibsinclude',['AddLibsInclude',['../d0/d75/classgdjs_1_1_exporter_helper.html#ab9c461030dec4f01d4fbcca3e374b7a0',1,'gdjs::ExporterHelper']]]
+  ['addlibsinclude',['AddLibsInclude',['../d0/d75/classgdjs_1_1_exporter_helper.html#aa7d0097723c4c5618e8fe2de03b0efc2',1,'gdjs::ExporterHelper']]]
 ];
