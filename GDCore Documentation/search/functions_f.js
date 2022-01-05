c7f044ff8af3a8a83de3c5d2dce5df00ca0b9b44
GDevelop documentation CI <ci-build@gdevelop-app.com>
2022-01-05 00:12:04 +0000

    Update documentation [ci skip]

--- GDCore Documentation/search/functions_f.js
+++ GDCore Documentation/search/functions_f.js
@@ -108,7 +108,7 @@ var searchData=
   ['setopenglznear',['SetOpenGLZNear',['../classgd_1_1_layout.html#a56c05800e64e15c2aa1e3b5b178ba0a1',1,'gd::Layout']]],
   ['setoptional',['SetOptional',['../classgd_1_1_parameter_metadata.html#ac09fe4cb772b5911e4dc700cb41d3343',1,'gd::ParameterMetadata']]],
   ['setorientation',['SetOrientation',['../classgd_1_1_project.html#a7eafcb2a559636e4694a55c817bc906d',1,'gd::Project']]],
-  ['setorigin',['SetOrigin',['../classgd_1_1_resource.html#a207d8146b1168b966bbb4a03cf640fed',1,'gd::Resource']]],
+  ['setorigin',['SetOrigin',['../classgd_1_1_events_functions_extension.html#a2c905b7081a9a70506e3674f12e528c2',1,'gd::EventsFunctionsExtension::SetOrigin()'],['../classgd_1_1_resource.html#a207d8146b1168b966bbb4a03cf640fed',1,'gd::Resource::SetOrigin()']]],
   ['setpackagename',['SetPackageName',['../classgd_1_1_project.html#ac7d556b9e3ed34867b8664e5844ae5b7',1,'gd::Project']]],
   ['setparameter',['SetParameter',['../classgd_1_1_instruction.html#a9caf7b28bdb2d087ada412515d776cec',1,'gd::Instruction']]],
   ['setparameterextrainfo',['SetParameterExtraInfo',['../classgd_1_1_instruction_metadata.html#a08fb6f94e5972ae9ded0de9a5f8545ae',1,'gd::InstructionMetadata']]],
