5f436d78683d612cb365e0e50a63acc4249f7a67
GDevelop documentation CI <ci-build@gdevelop-app.com>
2022-05-09 00:12:59 +0000

    Update documentation [ci skip]

--- GDJS Documentation/search/functions_7.js
+++ GDJS Documentation/search/functions_7.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['setcodenamespace',['SetCodeNamespace',['../d7/d4f/classgdjs_1_1_events_code_generator.html#acce6507d78b9e6552c646202c6d747dc',1,'gdjs::EventsCodeGenerator']]],
   ['setcodeoutputdirectory',['SetCodeOutputDirectory',['../dc/dda/classgdjs_1_1_exporter.html#a9c51541a1481d6bf610f15f1621e1ddd',1,'gdjs::Exporter::SetCodeOutputDirectory()'],['../d0/d75/classgdjs_1_1_exporter_helper.html#afeecd7cc427087d226bf9591954ef243',1,'gdjs::ExporterHelper::SetCodeOutputDirectory()']]],
+  ['setelectronremoterequirepath',['SetElectronRemoteRequirePath',['../db/dbe/structgdjs_1_1_preview_export_options.html#a3903ccd881c7ea0a205aa19086461cca',1,'gdjs::PreviewExportOptions']]],
   ['setexternallayoutname',['SetExternalLayoutName',['../db/dbe/structgdjs_1_1_preview_export_options.html#a26226ff501d285d19288143f77581548',1,'gdjs::PreviewExportOptions']]],
   ['setfullloadingscreen',['SetFullLoadingScreen',['../db/dbe/structgdjs_1_1_preview_export_options.html#a7bfbc8544c60894d6eb4575bf79b0889',1,'gdjs::PreviewExportOptions']]],
   ['setincludefilehash',['SetIncludeFileHash',['../db/dbe/structgdjs_1_1_preview_export_options.html#a4ad48abf2a9a6f3974b91c12ea1eb136',1,'gdjs::PreviewExportOptions']]],
