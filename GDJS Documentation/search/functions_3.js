0af2d465ebeae514b05b97cebbd07be4d2a5df62
GDevelop documentation CI <ci-build@gdevelop-app.com>
2023-01-03 00:12:27 +0000

    Update documentation [ci skip]

--- GDJS Documentation/search/functions_3.js
+++ GDJS Documentation/search/functions_3.js
@@ -7,8 +7,6 @@ var searchData=
   ['exportexternalsourcefiles',['ExportExternalSourceFiles',['../d0/d75/classgdjs_1_1_exporter_helper.html#a6f9aef14c2dfee84123d0c29f470b96c',1,'gdjs::ExporterHelper']]],
   ['exportfacebookinstantgamesfiles',['ExportFacebookInstantGamesFiles',['../d0/d75/classgdjs_1_1_exporter_helper.html#a749478cbbaec91e7ca2bf51447f9ecd5',1,'gdjs::ExporterHelper']]],
   ['exportincludesandlibs',['ExportIncludesAndLibs',['../d0/d75/classgdjs_1_1_exporter_helper.html#a4b7bf354abd3e2548eed82ddfd59f40a',1,'gdjs::ExporterHelper']]],
-  ['exportobjectandbehaviorsincludes',['ExportObjectAndBehaviorsIncludes',['../d0/d75/classgdjs_1_1_exporter_helper.html#a26da997eebf25f4c46d9c641a4f02e40',1,'gdjs::ExporterHelper']]],
-  ['exportobjectandbehaviorsrequiredfiles',['ExportObjectAndBehaviorsRequiredFiles',['../d0/d75/classgdjs_1_1_exporter_helper.html#a4361485916a1532fd290d151e41b6e9e',1,'gdjs::ExporterHelper']]],
   ['exportpixiindexfile',['ExportPixiIndexFile',['../d0/d75/classgdjs_1_1_exporter_helper.html#ae2aae031a52f88912d57853ed53db002',1,'gdjs::ExporterHelper']]],
   ['exportprojectdata',['ExportProjectData',['../d0/d75/classgdjs_1_1_exporter_helper.html#aecc3874fc14a7a8feb3a1afba7285177',1,'gdjs::ExporterHelper']]],
   ['exportprojectforpixipreview',['ExportProjectForPixiPreview',['../dc/dda/classgdjs_1_1_exporter.html#ae6c8aea4046fd6fae37a4e163cd2b968',1,'gdjs::Exporter::ExportProjectForPixiPreview()'],['../d0/d75/classgdjs_1_1_exporter_helper.html#a26a7da4237f55e55b078bdd585a39c4a',1,'gdjs::ExporterHelper::ExportProjectForPixiPreview()']]],
