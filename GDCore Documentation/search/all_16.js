9b717cead36af2a04dbd12c964c21597d44b7c1d
GDevelop documentation CI <ci-build@gdevelop-app.com>
2023-01-25 00:12:52 +0000

    Update documentation [ci skip]

--- GDCore Documentation/search/all_16.js
+++ GDCore Documentation/search/all_16.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['watermark',['Watermark',['../classgd_1_1_watermark.html',1,'gd']]],
   ['whileevent',['WhileEvent',['../classgd_1_1_while_event.html',1,'gd']]],
   ['wholeprojectrefactorer',['WholeProjectRefactorer',['../classgd_1_1_whole_project_refactorer.html',1,'gd']]],
   ['writer',['Writer',['../class_writer.html',1,'Writer&lt; OutputStream, SourceEncoding, TargetEncoding, StackAllocator, writeFlags &gt;'],['../class_writer.html#af4f54830d6927d9daf5bd53bfd134dd3',1,'Writer::Writer()']]],
--- GDCore Documentation/search/classes_15.js
+++ GDCore Documentation/search/classes_15.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['watermark',['Watermark',['../classgd_1_1_watermark.html',1,'gd']]],
   ['whileevent',['WhileEvent',['../classgd_1_1_while_event.html',1,'gd']]],
   ['wholeprojectrefactorer',['WholeProjectRefactorer',['../classgd_1_1_whole_project_refactorer.html',1,'gd']]],
   ['writer',['Writer',['../class_writer.html',1,'']]]
